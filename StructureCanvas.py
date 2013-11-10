bbd3fc9bbb2bb80cd8f018011d51058714ac53a4
cody bellitto <codybell@buffalo.edu>
2013-11-10 02:55:49 -0500

    more progress sorta

---
+++ StructureCanvas.py
@@ -0,0 +1,16 @@
+import Tkinter as tk
+
+class StructureCanvas(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas = tk.Canvas.__init__(self, *args, **kwargs)
+        canvas = tk.Canvas(self, width=800, height=500)
+        canvas.pack(side="top", fill="both", expand=True)
+        canvas_id = canvas.create_text(10, 10, anchor="nw")
+
+        canvas.itemconfig(canvas_id, text="this is the text")
+        canvas.insert(canvas_id, 12, "new ")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    StructureCanvas(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
