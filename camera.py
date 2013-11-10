06bf1f12f7ea32af1c3b3472826bd357c05d822a
Scott Florentino <smflorentino@email.fake>
2013-11-09 21:06:47 -0500

    number OCR working

--- camera.py
+++ camera.py
@@ -10,32 +10,24 @@ Put Your Code in process
 '''
 
 def process(img):
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	circles =  cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 40, np.array([]), 100, 40, 5, 300)
-	if circles is not None:
-		for c in circles[0]:
-			#Draw Circle
-			cv2.circle(img, (c[0],c[1]), c[2], (100,255,100),2)
-			#Draw enter
-			cv2.circle(img, (c[0],c[1]), 1, (100,100 ,255),2)
-	gray = np.float32(gray)
-	dst = cv2.cornerHarris(gray,4,3,0.04)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    circles =  cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 40, np.array([]), 100, 40, 5, 300)
+    if circles is not None:
+        for c in circles[0]:
+            #Draw Circle
+            cv2.circle(img, (c[0],c[1]), c[2], (100,255,100),2)
+            #Draw enter
+            cv2.circle(img, (c[0],c[1]), 1, (100,100 ,255),2)
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray,4,3,0.04)
 
-	#result is dilated for marking the corners, not important
-	dst = cv2.dilate(dst,None)
+    #result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst,None)
 
     # Threshold for an optimal value, it may vary depending on the image.
     img[dst>0.01*dst.max()]=[0,0,255]
     #findarrows(circles,img)
     cv2.imshow("Image Feed",img)
-
-
-        
-def findarrows(circles,img):
-        for c in circles[0]:
-                for d in circles[0]:
-                       # line=cv2.fitline([(c[0],c[1]),(d[0],d[1])],CV_DIST_L2,0,.01,.01)
-                        cv2.line(img, (c[0],c[1]), (d[0],d[1]), (200,200,50)) 
 
 
 def main():
