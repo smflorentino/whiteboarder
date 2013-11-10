69491628ad8baa4cf9a8eae165015732b64066dc
Scott Florentino <smflorentino@email.fake>
2013-11-10 03:00:15 -0500

    WE ALLL IT ON ONE SCREEN

--- edgedetection.py
+++ edgedetection.py
@@ -1,11 +1,17 @@
 import cv2
 import numpy as np
 import math
+import os
+import sys
+from common import mosaic
+
+from digits import *
 from matplotlib import pyplot as plt
 
 #Global Variables
 corners = []
-
+frame = None
+img = None
 
 #Utility Functions
 def slope(circle1, circle2):
@@ -206,7 +212,7 @@ def dotcount(points,dst,box):
 
 
 #CORNER DETECTION
-def getCornerList(img):
+def getCornerList():
   gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
   corners = cv2.goodFeaturesToTrack(gray,50,0.05,0)
@@ -214,24 +220,103 @@ def getCornerList(img):
 
   return corners;
 
-def process(vc,img):
+#NUMBER DETECTION
+def findNumbers():
+    print "starting...."
+    #blank = True
+    #while blank:
+    #    rval, frame = vc.read()
+    #    if frame is not None:
+    #        blank=False
+
+    classifier_fn = 'digits_svm.dat'
+    if not os.path.exists(classifier_fn):
+        print '"%s" not found, run digits.py first' % classifier_fn
+        return
+    model = SVM()
+    model.load(classifier_fn)
+
+    stop=True
+    while stop:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 31, 10)
+        bin = cv2.medianBlur(bin, 3)
+        contours, heirs = cv2.findContours( bin.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        try: heirs = heirs[0]
+        except: heirs = []
+
+        for cnt, heir in zip(contours, heirs):
+            _, _, _, outer_i = heir
+            if outer_i >= 0:
+                continue
+            x, y, w, h = cv2.boundingRect(cnt)
+            if not (16 <= h <= 64  and w <= 1.2*h):
+                continue
+            pad = max(h-w, 0)
+            x, w = x-pad/2, w+pad
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+
+            bin_roi = bin[y:,x:][:h,:w]
+            gray_roi = gray[y:,x:][:h,:w]
+
+            m = bin_roi != 0
+            if not 0.1 < m.mean() < 0.4:
+                continue
+            '''
+            v_in, v_out = gray_roi[m], gray_roi[~m]
+            if v_out.std() > 10.0:
+                continue
+            s = "%f, %f" % (abs(v_in.mean() - v_out.mean()), v_out.std())
+            cv2.putText(frame, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (200, 0, 0), thickness = 1)
+            '''
+
+            s = 1.5*float(h)/SZ
+            m = cv2.moments(bin_roi)
+            c1 = np.float32([m['m10'], m['m01']]) / m['m00']
+            c0 = np.float32([SZ/2, SZ/2])
+            t = c1 - s*c0
+            A = np.zeros((2, 3), np.float32)
+            A[:,:2] = np.eye(2)*s
+            A[:,2] = t
+            bin_norm = cv2.warpAffine(bin_roi, A, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+            bin_norm = deskew(bin_norm)
+            if x+w+SZ < frame.shape[1] and y+SZ < frame.shape[0]:
+                frame[y:,x+w:][:SZ, :SZ] = bin_norm[...,np.newaxis]
+
+            sample = preprocess_hog([bin_norm])
+            digit = model.predict(sample)[0]
+            cv2.putText(frame, '%d'%digit, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (200, 0, 0), thickness = 1)
+        stop=False
+
+
+        cv2.imshow("Image Feed", frame)
+        #cv2.imshow('bin', gray)
+        #ch = cv2.waitKey(1)
+        #if ch == 27:
+        #    break
+
+
+
+def process():
  
   #GET A NON BLANK IMAGE
-  blank = True
-  while blank:
-      rval, img = vc.read()
-      if img is not None:
-        print "not empty"
-        blank=False
+  #blank = True
+  #while blank:
+  #    rval, img = vc.read()
+  #    if img is not None:
+  #     print "not empty"
+  #      blank=False
 
   #CIRCLE DETECTION
+  print "img",img
   gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
   circles =  cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 40, np.array([]), 100, 40, 5, 300)
   if circles is None:
     return
 
   #CORNER DETECTION
-  corners = getCornerList(img)
+  corners = getCornerList()
 
 
   for c in circles[0]:
@@ -248,7 +333,10 @@ def process(vc,img):
         cv2.circle(img, (c[0],c[1]), 1, (100,100 ,255),1)
         #break
         cv2.line(img, (c[0],c[1]),(d[0],d[1]), (200,200,50))
-        cv2.imshow("Image Feed",img)
+        
+
+  findNumbers()
+  cv2.imshow("Image Feed",img)
         #break
         #r=30
         #theta=math.atan(-(d[0]-c[0])/(d[1]-c[1]))
@@ -275,20 +363,22 @@ def process(vc,img):
 def main():
   cv2.namedWindow("Image Feed")
   vc = cv2.VideoCapture(0)
-
+  global frame
+  global img
   rval, frame = vc.read()
 
   while True:
     if frame is not None:
       #Show Live Image
-      cv2.imshow("preview", frame)
+      cv2.imshow("Image Feed", frame)
+      img = frame
+      print "img",img,"frame",frame
       #Process it instead
-      process(vc,frame)
+      process()
     if frame is None:
       print "empty"
 
     rval, frame = vc.read()
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
